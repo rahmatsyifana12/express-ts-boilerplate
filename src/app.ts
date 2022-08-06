b694437aa55c75d2704b3588fc4fcc4cbbf64f99
rahmatsyifana12 <adean435@gmail.com>
2022-08-07 00:08:24 +0700

    Add port into app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import config from './configs/config';
 
 const app = express();
-const port = 5000;
+const port = config.port ?? 5000;
 
 app.use(express.json());
 app.use(cors());
