0e2f140252ee04b56d13054724abcd64588de352
rahmatsyifana12 <adean435@gmail.com>
2022-08-07 00:06:51 +0700

    Add config

---
+++ src/configs/config.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { env } = process;
+
+const config = {
+    db: {
+        host: env.DB_HOST!,
+        port: env.DB_PORT!,
+        database: env.DB_DATABASE!,
+        username: env.DB_USERNAME!,
+        password: env.DB_PASSWORD!
+    },
+    jwt: {
+        accessSecret: env.JWT_ACCESS_SECRET,
+        refreshSecret: env.JWT_REFRESH_SECRET,
+        accessExpire: env.JWT_ACCESS_EXPIRE,
+        refreshExpire: env.JWT_REFRESH_EXPIRE
+    },
+    hashRounds: 10,
+    port: env.PORT
+};
+
+export default config;
