e7571d77a0536e29e13e4317193a916be4d36963
rahmatsyifana12 <adean435@gmail.com>
2022-08-07 20:28:51 +0700

    Add error util for error handing

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,11 @@
 import type { Request, Response } from 'express';
+import { authService } from '../services/auth.service';
 import { validate } from '../utils/validate.util';
 import { createUserSchema } from '../validations/user.validate';
 
 async function register(req: Request, res: Response) {
     const body = validate(req, createUserSchema, 'body');
+    await authService.create(body);
 
     return res.status(200).json({
         status: 'success',
--- src/utils/validate.util.ts
+++ src/utils/validate.util.ts
@@ -1,5 +1,6 @@
 import type { Request } from 'express';
 import type { ObjectSchema } from 'joi';
+import { ResponseError } from './error.util';
 
 type ValidationType = 'body' | 'params';
 
@@ -12,7 +13,7 @@ export function validate<T>(
     const { value, error } = schema.validate(content);
 
     if (error) {
-        // throw error
+        throw new ResponseError('Invalid object value', 400);
     }
 
     return value;
