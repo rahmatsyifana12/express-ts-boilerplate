3173567f69cbff76c710bd1f675beba32788d23c
rahmatsyifana12 <adean435@gmail.com>
2022-08-07 20:49:09 +0700

    Update error handling

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,20 @@
 import type { Request, Response } from 'express';
 import { authService } from '../services/auth.service';
+import type { ResponseError } from '../utils/error.util';
 import { validate } from '../utils/validate.util';
 import { createUserSchema } from '../validations/user.validate';
 
 async function register(req: Request, res: Response) {
-    const body = validate(req, createUserSchema, 'body');
-    await authService.create(body);
+    try {
+        const body = validate(req, createUserSchema, 'body');
+        await authService.create(body);
+    } catch (error) {
+        const err = error as ResponseError;
+        return res.status(err.statusCode).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
 
     return res.status(200).json({
         status: 'success',
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,13 @@
 import { User } from '../database/entities/User';
+import { ResponseError } from '../utils/error.util';
 import type { CreateUserType } from '../validations/user.validate';
 
 class AuthService {
 
     async create(rawUser: CreateUserType) {
         const user = User.create({ ...rawUser });
+
+        throw new ResponseError('Testing', 500);
 
         await User.save(user);
     }
