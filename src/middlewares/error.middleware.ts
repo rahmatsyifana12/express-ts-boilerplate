18198f2e38e74a2e9afeb92ad4136f418cbef013
rahmatsyifana12 <adean435@gmail.com>
2022-08-08 19:59:42 +0700

    Change ResponseError params to StatusCodes and add common API errors

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -3,7 +3,7 @@ import { sendResponse } from '../utils/api.util';
 import type { ResponseError } from '../utils/error.util';
 
 async function errorHandling(
-    error: Error, _: Request, res: Response, next:NextFunction) {
+    error: Error, _: Request, res: Response, next: NextFunction) {
 
     const err = error as ResponseError;
 }
--- src/utils/error.util.ts
+++ src/utils/error.util.ts
@@ -1,10 +1,45 @@
+import { StatusCodes } from 'http-status-codes';
+
 export class ResponseError extends Error {
 
-    statusCode: number;
+    statusCode: StatusCodes;
 
-    constructor(message: string, statusCode: number) {
+    constructor(message: string, statusCode: StatusCodes) {
         super(message);
         this.statusCode = statusCode;
     }
 
 }
+
+/**
+ * Common API errors
+ */
+export const Errors = {
+    /**
+     * Internal server error / Unexpected error
+     */
+    SERVER: new ResponseError(
+        'Unexpected server error',
+        StatusCodes.INTERNAL_SERVER_ERROR),
+
+    /**
+     * User doesn't have JWT or authentication token
+     */
+    NO_SESSION: new ResponseError(
+        "You don't have an account session",
+        StatusCodes.UNAUTHORIZED),
+
+    /**
+     * User doesn't have the permission
+     */
+    NO_PERMISSION: new ResponseError(
+        "You don't have the permission to access this content",
+        StatusCodes.FORBIDDEN),
+
+    /**
+     * Cannot find the user it's trying to find
+     */
+    USER_NOT_FOUND: new ResponseError(
+        'Cannot find user',
+        StatusCodes.NOT_FOUND),
+};
