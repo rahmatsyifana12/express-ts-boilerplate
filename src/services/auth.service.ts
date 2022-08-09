ff2deb678235ceca7118de2e7a64d3890f67d5ab
rahmatsyifana12 <adean435@gmail.com>
2022-08-09 21:45:58 +0700

    Update register service logic: add hash password

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,12 +1,29 @@
+import bcrypt from 'bcrypt';
+import { StatusCodes } from 'http-status-codes';
 import { User } from '../database/entities/user.entity';
+import { ResponseError } from '../utils/error.util';
 import type { CreateUserType } from '../validations/user.validate';
+import config from '../configs/config';
 
 class AuthService {
 
     async create(rawUser: CreateUserType) {
         const user = User.create({ ...rawUser });
 
+        const foundUser = await User.findOneBy({ email: user.email });
+        if (foundUser) {
+            throw new ResponseError(
+                'This email is already registered',
+                StatusCodes.BAD_REQUEST);
+        }
+
+        user.password = await this.hashPassword(user.password);
+
         await User.save(user);
+    }
+
+    async hashPassword(password: string) {
+        return bcrypt.hash(password, config.hashRounds);
     }
 
 }
