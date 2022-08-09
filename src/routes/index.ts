31c5655a5d9a31cd07de34bfe0fdeb6f7de4e5c9
rahmatsyifana12 <adean435@gmail.com>
2022-08-10 00:46:07 +0700

    Add login routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,6 @@ import { authController } from '../controllers/auth.controller';
 const router = Router();
 
 router.post('/v1/auth/register', authController.register);
+router.post('/v1/auth/login', authController.login);
 
 export default router;
