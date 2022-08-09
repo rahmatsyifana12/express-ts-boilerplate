f311207a89ce50d75eaaca473058d0ff5443c288
rahmatsyifana12 <adean435@gmail.com>
2022-08-10 00:38:43 +0700

    Add auth typings and generate token function

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,11 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { StatusCodes } from 'http-status-codes';
 import { User } from '../database/entities/user.entity';
 import { ResponseError } from '../utils/error.util';
 import type { RegisterType, LoginType } from '../validations/user.validate';
 import config from '../configs/config';
+import type { TokenType } from '../typings/auth';
 
 class AuthService {
 
@@ -23,11 +25,44 @@ class AuthService {
     }
 
     async login({ email, password }: LoginType) {
+        const user = await User.findOneBy({ email });
+        if (!user) {
+            throw new ResponseError(
+                'Account does not exists!', StatusCodes.BAD_REQUEST);
+        }
 
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) {
+            throw new ResponseError(
+                'Password is incorrect!', StatusCodes.BAD_REQUEST);
+        }
     }
 
     async hashPassword(password: string) {
         return bcrypt.hash(password, config.hashRounds);
+    }
+
+    async generateToken(user: User, tokenType: TokenType) {
+        const payload = { id: user.id };
+        const options: jwt.SignOptions = {};
+
+        let tokenSecret;
+
+        if (tokenType === 'ACCESS') {
+            options.expiresIn = config.jwt.accessExpire;
+            tokenSecret = config.jwt.accessSecret;
+        } else {
+            options.expiresIn = config.jwt.refreshExpire;
+            tokenSecret = config.jwt.refreshSecret;
+        }
+
+        const token = jwt.sign(payload, tokenSecret, options);
+
+        if (tokenType === 'REFRESH') {
+            user.refreshToken = token;
+        }
+
+        return token;
     }
 
 }
