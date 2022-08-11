00c64c690de089fb9dffae363070043ae834186e
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2022-08-11 18:30:26 +0700

    Add add todo controller

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,6 +1,22 @@
+import type { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { todoService } from '../services/todo.service';
+import { sendResponse } from '../utils/api.util';
+import { validate } from '../utils/validate.util';
+import { addTodoScehma } from '../validations/todo.validate';
+
 class TodoController {
 
+    async add(req: Request, res: Response) {
+        const body = validate(req, addTodoScehma, 'body');
+        await todoService.add(body, 1); // userId still dummy
 
+        return sendResponse(res, {
+            statusCode: StatusCodes.CREATED,
+            success: true,
+            message: 'Successfully created a todo'
+        });
+    }
 
 }
 
