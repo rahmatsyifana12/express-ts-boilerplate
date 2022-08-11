0eb4a3719696102338acea97656a0aa65c01000c
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2022-08-11 18:31:29 +0700

    Add add todo routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import Router from 'express';
 import { authController } from '../controllers/auth.controller';
+import { todoController } from '../controllers/todo.controller';
 import authenticate from '../middlewares/authenticate.middleware';
 
 const router = Router();
 
 router.post('/v1/auth/register', authController.register);
 router.post('/v1/auth/login', authController.login);
-router.post('/auth/refresh', authenticate('REFRESH'), authController.refresh);
+router.post('v1/auth/refresh', authenticate('REFRESH'), authController.refresh);
+
+router.post('/v1/todos', authenticate('ACCESS'), todoController.add);
 
 export default router;
