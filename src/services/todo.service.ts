25a9478853d13f0ea523bbc985b52f4c3a1224d1
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2022-08-11 18:27:31 +0700

    Add add todo service

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,6 +1,13 @@
+import { Todo } from '../database/entities/todo.entity';
+import type { AddTodoType } from '../validations/todo.validate';
+
 class TodoService {
 
+    async add(rawTodo: AddTodoType, userId: number) {
+        const todo = Todo.create({ userId, ...rawTodo });
 
+        await Todo.save(todo);
+    }
 
 }
 
