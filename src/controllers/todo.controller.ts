afed696addf57864a5203103c8835be637d9c342
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2022-08-11 18:38:11 +0700

    Add get token payload into add todo controller

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { authService } from '../services/auth.service';
 import { todoService } from '../services/todo.service';
 import { sendResponse } from '../utils/api.util';
 import { validate } from '../utils/validate.util';
@@ -8,8 +9,10 @@ import { addTodoScehma } from '../validations/todo.validate';
 class TodoController {
 
     async add(req: Request, res: Response) {
+        const userPayload = await authService.getTokenPayload(req, 'ACCESS');
         const body = validate(req, addTodoScehma, 'body');
-        await todoService.add(body, 1); // userId still dummy
+
+        await todoService.add(body, userPayload!.userId); // userId still dummy
 
         return sendResponse(res, {
             statusCode: StatusCodes.CREATED,
