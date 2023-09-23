9c7dbc4b8ddbb8181089a0aad9f714a9725b1cf9
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2023-09-23 23:07:49 +0700

    Add bodyparser and timeout

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import timeout from 'connect-timeout';
 
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
 app.use(cors());
+app.use(timeout(20000));
 
 export default app;
