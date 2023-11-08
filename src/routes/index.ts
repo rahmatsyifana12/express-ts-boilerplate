8317b2c5cbc1eef617188f1fa596057d149a0282
Aan Rahmat <70148910+rahmatsyifana12@users.noreply.github.com>
2023-11-08 22:03:31 +0700

    Tidy up code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,19 +10,16 @@ const router = Router();
 router.post('/v1/auth/register', authController.register);
 router.post('/v1/auth/login', authController.login);
 router.put('/v1/auth/logout', authenticate('REFRESH'), authController.logout);
-router.post('/v1/auth/refresh', authenticate('REFRESH'),
-    authController.refresh);
+router.post('/v1/auth/refresh', authenticate('REFRESH'), authController.refresh);
 
 // users
-router.get('/v1/users/profile', authenticate('ACCESS'),
-    userController.getProfile);
+router.get('/v1/users/profile', authenticate('ACCESS'), userController.getProfile);
 router.put('/v1/users', authenticate('ACCESS'), userController.update);
 
 // todos
 router.post('/v1/todos', authenticate('ACCESS'), todoController.add);
 router.get('/v1/todos', authenticate('ACCESS'), todoController.getAll);
 router.put('/v1/todos/:todoId', authenticate('ACCESS'), todoController.update);
-router.delete('/v1/todos/:todoId',
-    authenticate('ACCESS'), todoController.delete);
+router.delete('/v1/todos/:todoId', authenticate('ACCESS'), todoController.delete);
 
 export default router;
